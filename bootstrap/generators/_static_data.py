be527692a2909993de75c429d34de4848b49736a
Leonhard S <leonhard-sei@outlook.com>
2024-03-03 11:52:48 +0100

    Fix hard-coded JSON file path

--- bootstrap/generators/_static_data.py
+++ bootstrap/generators/_static_data.py
@@ -13,5 +13,5 @@ _STATIC_DATA = pathlib.Path(__file__).parents[2] / 'static'
 
 def get_static_data(name: str) -> list[dict[str, typing.Any]]:
     """Load static data from the in-repo JSON files."""
-    with open(_STATIC_DATA / 'outfit_resources.json', 'r', encoding='utf8') as file_:
+    with open(_STATIC_DATA / f'{name}.json', 'r', encoding='utf8') as file_:
         return json.load(file_)
